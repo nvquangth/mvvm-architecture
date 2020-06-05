65ff3d89c29cf464a04a0f044a4cf5b499f85669
quangnv <nguyenvanquang96th@gmail.com>
2020-06-05 08:02:21 +0700

    Add BaseViewModel

--- app/src/main/java/com/example/mvvmarchitecture/base/BaseViewModel.kt
+++ app/src/main/java/com/example/mvvmarchitecture/base/BaseViewModel.kt
@@ -1,18 +1,78 @@
 package com.example.mvvmarchitecture.base
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.mvvmarchitecture.data.remote.convertToBaseException
+import com.example.mvvmarchitecture.util.SingleLiveEvent
 import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.net.ConnectException
+import java.net.SocketTimeoutException
+import java.net.UnknownHostException
 
 /**
  * Created by Quang Nguyen on 6/3/20.
  */
 abstract class BaseViewModel: ViewModel() {
 
+    /**
+     * This variable is private because we do not want to expose MutableLiveData
+     *
+     * MutableLiveData allows anyone to set value, and ViewModel is the only class that should be
+     * setting values.
+     */
+    private val _isLoading = MutableLiveData<Boolean>(false)
+    val isLoading: LiveData<Boolean>
+        get() = _isLoading
+
+    private val _errorMessage = SingleLiveEvent<String>()
+    val errorMessage: LiveData<String>
+        get() = _errorMessage
+
+    private val _noInternetConnectionEvent = SingleLiveEvent<Unit>()
+    val noInternetConnectionEvent: LiveData<Unit>
+        get() = _noInternetConnectionEvent
+
+    private val _connectTimeoutEvent = SingleLiveEvent<Unit>()
+    val connectTimeoutEvent: LiveData<Unit>
+        get() = _connectTimeoutEvent
+
+    private val _forceUpdateAppEvent = SingleLiveEvent<Unit>()
+    val forceUpdateAppEvent: LiveData<Unit>
+        get() = _forceUpdateAppEvent
+
+    private val _serverMaintainEvent = SingleLiveEvent<Unit>()
+    val serverMaintainEvent: LiveData<Unit>
+        get() = _serverMaintainEvent
+
+    private val _unknownErrorEvent = SingleLiveEvent<Unit>()
+    val unknownErrorEvent: LiveData<Unit>
+        get() = _unknownErrorEvent
+
     private val coroutineExceptionHandler = CoroutineExceptionHandler { context, throwable ->
-        onError(throwable)
+        viewModelScope.launch {
+            onError(throwable)
+        }
     }
 
-    private fun onError(throwable: Throwable) {
+    open suspend fun onError(throwable: Throwable) {
+        withContext(Dispatchers.Main) {
+            when(throwable) {
+                is UnknownHostException -> _noInternetConnectionEvent.call()
+
+                is ConnectException -> _noInternetConnectionEvent.call()
+
+                is SocketTimeoutException -> _connectTimeoutEvent.call()
+
+                else -> {
+                    val baseException = convertToBaseException(throwable)
+                }
+            }
+        }
 
     }
 }
---
+++ app/src/main/java/com/example/mvvmarchitecture/util/SingleLiveEvent.kt
@@ -0,0 +1,35 @@
+package com.example.mvvmarchitecture.util
+
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import java.util.concurrent.atomic.AtomicBoolean
+
+/**
+ * Created by Quang Nguyen on 6/5/20.
+ */
+class SingleLiveEvent<T> : MutableLiveData<T>() {
+
+    private val pending = AtomicBoolean(false)
+
+    override fun setValue(value: T?) {
+        pending.set(true)
+        super.setValue(value)
+    }
+
+    override fun observe(owner: LifecycleOwner, observer: Observer<in T>) {
+        super.observe(owner, Observer {
+            if (pending.compareAndSet(true, false)) {
+                observer.onChanged(it)
+            }
+        })
+    }
+
+    /**
+     * Single event for no data input. Make call more clear
+     * Example: navigation with no data: SingleLiveEvent<Unit>()
+     */
+    fun call() {
+        value = null
+    }
+}
