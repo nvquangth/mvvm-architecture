874ac9d77142fca2f7826f1d7addad0abbb685ca
quangnv <nguyenvanquang96th@gmail.com>
2020-06-29 12:48:12 +0700

    Category screen

--- app/src/main/java/com/example/mvvmarchitecture/utils/BindingUtils.kt
+++ app/src/main/java/com/example/mvvmarchitecture/utils/BindingUtils.kt
@@ -2,6 +2,9 @@ package com.example.mvvmarchitecture.utils
 
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
+import com.example.mvvmarchitecture.BuildConfig
 
 /**
  * Created by Quang Nguyen on 6/29/20.
@@ -11,5 +14,16 @@ import androidx.databinding.BindingAdapter
 fun ImageView.setGlideUrl(url: String?) {
     GlideApp.with(context)
         .load(url)
+        .into(this)
+}
+
+@BindingAdapter("glideUrlCategory")
+fun ImageView.setImageUrlCategory(url: List<String>?) {
+    val name = BuildConfig.BASE_URL + url?.get((0..2).random())?.apply {
+        substring(0, this.length - 1)
+    }
+    GlideApp.with(context)
+        .load(name)
+        .apply(RequestOptions.bitmapTransform(RoundedCorners(Constants.RADIUS_CATEGORY_IMAGE)))
         .into(this)
 }
--- app/src/main/java/com/example/mvvmarchitecture/utils/Constants.kt
+++ app/src/main/java/com/example/mvvmarchitecture/utils/Constants.kt
@@ -10,5 +10,8 @@ object Constants {
     const val LIMIT_DEFAULT = 50
     const val OFFSET_DEFAULT = 0
 
+    const val GRID_RECYCLER_CATEGORY = 2
+    const val RADIUS_CATEGORY_IMAGE = 16
     const val SPACE_CATEGORY = 4F
+
 }
